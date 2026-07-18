53aee8b588867f087f067263de52506c0e738e73
Rudraksh <135806983+Rudraksh2605@users.noreply.github.com>
2026-07-18 11:04:21 +0530

    Time: 6 ms (18.90%) | Memory: 45.8 MB (7.89%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -4,14 +4,11 @@ class Solution {
         int large = nums[nums.length-1];
         int small = nums[0];
         boolean[] dp = new boolean[large+1];
-        for(int j = 0; j <= large; j++) dp[j] = false;
+        int ans = 1;
         for(int i = 2; i <= large; i++){
             if(large % i == 0 && small % i == 0) dp[i] = true;
-        }
-        int ans = 1;
-        for(int i = 0; i <= large; i++) {
             if(dp[i] == true) ans = Math.max(ans, i);
-        } 
+        }
 
         return ans;
     }
