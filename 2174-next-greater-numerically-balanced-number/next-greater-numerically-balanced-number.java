90d353f6eb7bb6b5585f2b89c52f4cb149cca647
Rudraksh <135806983+Rudraksh2605@users.noreply.github.com>
2026-07-29 22:42:07 +0530

    Time: 92 ms (40.84%) | Memory: 46.5 MB (12.04%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        int num = n+1;
+
+        while(true){
+            if(isbal(num)){
+                return num;
+            }
+            num++;
+        }
+    }
+
+    private boolean isbal(int num){
+        int[] freq = new int[10];
+        int temp = num;
+
+        while(temp > 0){
+            freq[temp%10]++;
+            temp /= 10;
+        }
+
+        for(int i = 0; i < 10; i++){
+            if(freq[i] != 0 && freq[i] != i){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
