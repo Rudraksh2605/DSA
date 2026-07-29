8b497817b52dbac9c655cb04c3f0bfad14eb72d7
Rudraksh <135806983+Rudraksh2605@users.noreply.github.com>
2026-07-29 21:14:21 +0530

    Time: 39 ms (23.38%) | Memory: 46.5 MB (71.43%) - LeetSync

--- 2810-collecting-chocolates/collecting-chocolates.java
+++ 2810-collecting-chocolates/collecting-chocolates.java
@@ -2,13 +2,13 @@ class Solution {
     public long minCost(int[] nums, int x) {
         long res = Long.MAX_VALUE;
         int n = nums.length;
-        int[] min = new int[n];
-        Arrays.fill(min, Integer.MAX_VALUE);
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         for (int k = 0; k < n; k++) {
             long sum = (long) k * x;
             for (int i = 0; i < n; i++) {
-                min[i] = Math.min(min[i], nums[(i + k) % n]);
-                sum += min[i];
+                dp[i] = Math.min(dp[i], nums[(i + k) % n]);
+                sum += dp[i];
             }
             res = Math.min(res, sum);
         }
